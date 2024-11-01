d0be59f865efa9fbc41598a0d0bb94b32692f2a3
Neelesh Chandola <neelesh.chandola@outlook.com>
2024-11-01 10:14:23 +0000

    cat-file -p <blob sha>

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import java.io.File
+import java.util.zip.InflaterInputStream
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
@@ -7,15 +8,30 @@ fun main(args: Array<String>) {
         exitProcess(1)
     }
 
-    if (args[0] == "init") {
-        val gitDir = File(".git")
-        gitDir.mkdir()
-        File(gitDir, "objects").mkdir()
-        File(gitDir, "refs").mkdir()
-        File(gitDir, "HEAD").writeText("ref: refs/heads/master\n")
-        println("Initialized git directory")
-    } else {
-        println("Unknown command: ${args[0]}")
-        exitProcess(1)
+    when (args[0]) {
+        "init" -> {
+            val gitDir = File(".git")
+            gitDir.mkdir()
+            File(gitDir, "objects").mkdir()
+            File(gitDir, "refs").mkdir()
+            File(gitDir, "HEAD").writeText("ref: refs/heads/master\n")
+            println("Initialized git directory")
+        }
+
+        "cat-file" -> {
+            assert(args[1] == "-p")
+            val blobSha = args[2]
+            val file = File(".git/objects/${blobSha.substring(0..1)}/${blobSha.substring(2)}")
+            val contents = zlibDecode(file.readBytes()).substringAfter(0.toChar())
+            print(contents)
+        }
+
+        else -> {
+            println("Unknown command: ${args[0]}")
+            exitProcess(1)
+        }
     }
 }
+
+fun zlibDecode(data: ByteArray) =
+    InflaterInputStream(data.inputStream()).bufferedReader().readText()
